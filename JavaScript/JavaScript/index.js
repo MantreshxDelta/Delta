beeab030d366894c02b4c64a9cdc7a3f495444e9
Liladhar <mantresh19@gmail.com>
2025-12-26 12:57:22 +0000

    Template literals

---
+++ JavaScript/JavaScript/index.js
@@ -0,0 +1,5 @@
+console.log("Hello world!")
+let pencilprice = 10;
+let erasorprice = 5;
+// let output = "The total price is: " + (pencilprice + erasorprice) + "£".
+console.log(`The total price is : ${pencilprice + erasorprice} Rupees.`);
