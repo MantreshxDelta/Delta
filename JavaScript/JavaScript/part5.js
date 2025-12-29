7beecb11853da8ae45c250dc6ac17dd0cfb86196
Liladhar <mantresh19@gmail.com>
2025-12-29 10:14:57 +0000

    Add Twitter post object literal with user details and engagement metrics

--- JavaScript/JavaScript/part5.js
+++ JavaScript/JavaScript/part5.js
@@ -4,3 +4,12 @@
 //     age: 23,
 //     clg: 'Apna clg'
 // };
+
+// Twitter post
+let post = {
+    Username: 'Mantresh',
+    Content: 'Coding',
+    Likes: 200,
+    Repost: 53,
+    Tags: 3
+};
