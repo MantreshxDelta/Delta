77dc46d28493b25f2fac0763afa834604e6ea35b
Liladhar <mantresh19@gmail.com>
2025-12-30 16:55:42 +0000

    Add HTML structure and JavaScript functions file for project

---
+++ JavaScript/JavaScript/functions.js
@@ -0,0 +1,20 @@
+function helo() {
+    console.log("Hello");
+}
+
+// function print1to5() {
+//     for (i=0; i<=5; i++) {
+//         console.log(i);
+//     }
+// }
+
+// function ifelse() {
+//     for (let i=0; i<=5; i++) {
+//         console.log(i);
+//         if (i==3) {
+//             break;
+//         }
+//     }
+// }
+
+// Qs
