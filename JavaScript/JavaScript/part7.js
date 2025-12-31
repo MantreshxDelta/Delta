f5302f883399efc63c6a0663707c4f8f1915bb4f
Liladhar <mantresh19@gmail.com>
2025-12-31 14:39:46 +0000

    try-catch function in part7.js

--- JavaScript/JavaScript/part7.js
+++ JavaScript/JavaScript/part7.js
@@ -1,13 +1,24 @@
 // This function
-const student = {
-    name: 'Mantresh',
-    age: 20,
-    city: 'Nsk',
-    eng: 20,
-    math: 89,
-    phy: 44,
-    getavg(){
-        let avg = (this.eng + this.math + this.phy) / 3;
-        console.log(`${this.name} got avg marks = ${avg}`);
-    }
-}
+// const student = {
+//     name: 'Mantresh',
+//     age: 20,
+//     city: 'Nsk',
+//     eng: 20,
+//     math: 89,
+//     phy: 44,
+//     getavg(){
+//         let avg = (this.eng + this.math + this.phy) / 3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
+
+// Try and Catch Function
+// console.log('Hellow');
+// console.log('Hellow');
+// try {
+//     console.log(a);
+// } catch(err) {
+//     console.log(err);
+// }
+// console.log('Hellow2');
+// console.log('Hellow2');
