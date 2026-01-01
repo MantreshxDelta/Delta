15f587b48a8543048b363d082c78caa215ef4354
Liladhar <mantresh19@gmail.com>
2026-01-01 13:17:01 +0000

    Add HTML structure and JavaScript file for part 8

---
+++ JavaScript/JavaScript/part8.js
@@ -0,0 +1,21 @@
+// For each
+// let arr = [1,2,3,4];
+
+// arr.forEach((el) => {
+//     console.log(el);
+// })
+
+// arr.forEach(function(el){
+//     console.log(el);
+// })
+
+let arr = [{
+    name: 'Prabhu',
+    Marks: 89,
+}, {
+    name: 'Prabhu',
+    Marks: 89,
+}, {
+    name: 'Prabhu',
+    Marks: 89,
+}];
