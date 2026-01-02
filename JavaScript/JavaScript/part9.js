db298331d2a3b8a7d0f0d0b1ef033ca233e77f7c
Liladhar <mantresh19@gmail.com>
2026-01-02 17:57:22 +0000

    Add initial implementation for part9.js with image and description logging

---
+++ JavaScript/JavaScript/part9.js
@@ -0,0 +1,8 @@
+document.getElementById("mainImg")
+let imgobj = document.getElementById("mainImg")
+console.dir(imgobj.src)
+
+// imgobj.src = "..//assets/creation_1.png"
+
+document.getElementById("description")
+console.dir(document.getElementById('description'))
