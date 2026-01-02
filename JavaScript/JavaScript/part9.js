b403d0404684df7709e82caa372bc86575c4fc2d
Liladhar <mantresh19@gmail.com>
2026-01-02 18:27:00 +0000

    Comment out image and description logging code in part9.js for clarity

--- JavaScript/JavaScript/part9.js
+++ JavaScript/JavaScript/part9.js
@@ -1,8 +1,24 @@
-document.getElementById("mainImg")
-let imgobj = document.getElementById("mainImg")
-console.dir(imgobj.src)
+// document.getElementById("mainImg")
+// let imgobj = document.getElementById("mainImg")
+// console.dir(imgobj.src)
 
 // imgobj.src = "..//assets/creation_1.png"
 
-document.getElementById("description")
-console.dir(document.getElementById('description'))
+// document.getElementById("description")
+// console.dir(document.getElementById('description'))
+
+// Class name
+
+// document.getElementsByClassName('oldImg');
+// console.dir(document.getElementsByClassName("oldImg"))
+
+// document.getElementsByClassName('oldImg'.classList);
+// console.dir(document.getElementsByClassName('oldImg').src)
+
+// document.getElementsByClassName('oldImg')
+// console.dir(document.getElementsByClassName('oldImg'))
+
+// let source = document.getElementsByClassName('oldImg');
+// for (i=0; i<source.length; i++) {
+//     console.log(document.getElementsByClassName('oldImg')[i])
+// }
