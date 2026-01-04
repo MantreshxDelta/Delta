bea5abec3cca20f9a6c7d4c88548762f769a2818
Liladhar <mantresh19@gmail.com>
2026-01-04 15:07:58 +0000

    Refactor part10.js to use addEventListener for button click events and clean up commented code

--- JavaScript/JavaScript/part10.js
+++ JavaScript/JavaScript/part10.js
@@ -5,12 +5,28 @@
 //     console.log('Button was clicked');
 // }
 
+// let btns = document.querySelectorAll('button');
+// for (btn of btns) {
+//     btn.onclick = sayhello;
+//     // btn.onmouseenter = sayhello;
+// }
+// function sayhello() {
+//     console.log('Hellow')
+// }
+
+// Event Listners
+// Used to write multiple functions like eg onclick you 
+// can only write this funciton one time but with event listners
+// you can write onclick lke functions as many times as you want
+
 let btns = document.querySelectorAll('button');
 for (btn of btns) {
-    btn.onclick = sayhello;
-    // btn.onmouseenter = sayhello;
+    btn.addEventListener('click', sayhello);
+    btn.addEventListener('click', sayhi);
 }
-function sayhello() {
-    console.log('Hellow')
+function sayhello(){
+    console.log('Hello');
 }
-
+function sayhi(){
+    console.log('Hel');
+}
