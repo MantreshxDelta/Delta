f03afdbb8245c790fa20f0ff1cbc4eb64abe7704
Liladhar <mantresh19@gmail.com>
2026-01-04 16:27:55 +0000

    Add CSS styles and HTML structure for part10, including random color functionality

--- JavaScript/JavaScript/part10.js
+++ JavaScript/JavaScript/part10.js
@@ -19,14 +19,36 @@
 // can only write this funciton one time but with event listners
 // you can write onclick lke functions as many times as you want
 
-let btns = document.querySelectorAll('button');
-for (btn of btns) {
-    btn.addEventListener('click', sayhello);
-    btn.addEventListener('click', sayhi);
-}
-function sayhello(){
-    console.log('Hello');
-}
-function sayhi(){
-    console.log('Hel');
-}
+// let btns = document.querySelectorAll('button');
+// for (btn of btns) {
+//     btn.addEventListener('click', sayhello);
+//     btn.addEventListener('click', sayhi);
+// }
+// function sayhello(){
+//     console.log('Hello');
+// }
+// function sayhi(){
+//     console.log('Hel');
+// }
+
+// Generate random color
+
+// let btn = document.querySelector('button');
+// btn.addEventListener('click', function () {
+//     let h1 = document.querySelector('h1');
+//     let randomcolor = getRandomColor();
+//     h1.innerText = randomcolor;
+
+//     let div = document.querySelector('div')
+//     div.style.backgroundColor = randomcolor;
+//     console.log('color updated')
+// })
+
+// function getRandomColor() {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
+
+//     let color = `rgb(${red}, ${green}, ${blue})`;
+//     return color;
+// }
