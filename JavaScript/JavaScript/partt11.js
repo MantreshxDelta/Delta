473d2f19b01a92155905ec090b7005c26b733da9
Liladhar <mantresh19@gmail.com>
2026-01-07 12:03:12 +0000

    Refactor savetoDb function: convert callback implementation to use Promises for better readability and error handling

--- JavaScript/JavaScript/partt11.js
+++ JavaScript/JavaScript/partt11.js
@@ -10,3 +10,54 @@
 // three();
 
 // Callback hell
+
+// h1 = document.querySelector('h1');
+
+// function changecolor(color,delay,nextColorChange) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         if (nextColorChange) nextColorChange();
+//     },delay)
+// }
+
+// changecolor('red', 1000, () => {
+//     changecolor('darkgreen', 1000, () => {
+//         changecolor('darkgreen', 1000)
+//     });
+// }); 
+
+// Promises
+// Worst case
+// function savetoDb (data, success, failure) {
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//     if (internetSpeed > 4){
+//         success();
+//     } else {
+//         failure();
+//     }
+// }
+
+// savetoDb("Apna college", () => {
+//     console.log("Success : Your data saved");
+//     savetoDb('Hello world', () => {
+//         console.log('Success2: Your data saved');
+//         savetoDb('HOOOOOOO', () => {
+//             console.log("Success3: Your data has been saved");
+//         }, () => {
+//             console.log("Failure3: Your data didn't saved");
+//         })
+//     }, () => {
+//         console.log("Failure2: Your data didn't saved");
+//     })
+// }, () => {
+//     console.log("Failed: to upload data")
+// })
+
+function savetoDb (data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if (internetSpeed > 4) {
+            resolve("Resolved");
+        } else (reject("Rejected"))
+    });
+}
