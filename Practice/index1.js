dc7fbdad127ea983fbc80f3a334e82f69c561030
Liladhar <mantresh19@gmail.com>
2026-01-11 14:31:39 +0000

    Practiced old question

--- Practice/index1.js
+++ Practice/index1.js
@@ -1,9 +1,9 @@
 // Smart Counter
+// const increaseBtn = document.getElementById("increaseBtn");
+// const decreaseBtn = document.getElementById("decreaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
 // const countEl = document.getElementById("count");
-// const messageEl = document.getElementById('message');
-// const increaseBtn = document.getElementById("increase");
-// const resetBtn = document.getElementById("reset");
-// const decreaseBtn = document.getElementById("decrease");
+// const messageEl = document.getElementById("message");
 
 // let count = 0;
 
@@ -22,37 +22,69 @@
 //     increaseBtn.disabled = count === 10;
 
 //     if (count === 10) {
-//         messageEl.textContent = 'Maximum Limit Reached';
+//         messageEl.textContent = "Maximum limit reached";
 //     } else if (count === 0) {
-//         messageEl.textContent = 'Minimum Limit Reached';
-//     } else messageEl.textContent = '';
-// };
-
-// increaseBtn.addEventListener("click", () => {
-//     if (count < 10) {
-//         count++;
-//         updateUI();
+//         messageEl.textContent = "Minimum limit reached"
+//     } else {
+//         messageEl.textContent = "";
 //     }
-// });
-
-// decreaseBtn.addEventListener("click", () => {
-//     if (count > 0){
-//         count--;
+// }
+//     increaseBtn.addEventListener('click', () => {
+//         if (count < 10){
+//             count++;
+//             updateUI();
+//         }
+//     })
+//     decreaseBtn.addEventListener('click', () => {
+//         if (count > 0) {
+//             count--;
+//             updateUI();
+//         }
+//     })
+//     resetBtn.addEventListener('click', () => {
+//         count = 0;
 //         updateUI();
-//     }
-// });
-
-// resetBtn.addEventListener("click", () => {
-//     count = 0;
+//     })
 //     updateUI();
-// })
-
-// updateUI();
-
 
 // Password Strength Check
-const passwordInput = document.getElementById("password");
-const strengthText = document.getElementById("strength");
+// const passwordInput = document.getElementById("password");
+// const strengthText = document.getElementById("strength");
+
+// passwordInput.addEventListener('input', () => {
+//     const password = passwordInput.value;
+
+//     if (password.length === 0) {
+//         strengthText.textContent = "Strength: ";
+//         strengthText.style.color = "black";
+//         return;
+//     }
+//     if (password.length < 6) {
+//         strengthText.textContent = "Strength: Weak";
+//         strengthText.style.color = "red";
+//     } else if (isOnlyLetters(password)) {
+//         strengthText.textContent = "Strength: Medium";
+//         strengthText.style.color = "orange";
+//     } else {
+//         strengthText.textContent = "Strength: Strong";
+//         strengthText.style.color = "green"
+//     }
+// });
+
+// const isOnlyLetters = (Text) => {
+//     for (let char of Text) {
+//         if (!isNaN(char)) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+// Pswrd
+
+const passwordInput = document.getElementById('password');
+const strengthText = document.getElementById('strength');
 
 passwordInput.addEventListener('input', () => {
     const password = passwordInput.value;
@@ -61,7 +93,7 @@ passwordInput.addEventListener('input', () => {
         strengthText.textContent = "Strength: ";
         strengthText.style.color = "black";
         return;
-    }
+    } 
     if (password.length < 6) {
         strengthText.textContent = "Strength: Weak";
         strengthText.style.color = "red";
@@ -70,15 +102,15 @@ passwordInput.addEventListener('input', () => {
         strengthText.style.color = "orange";
     } else {
         strengthText.textContent = "Strength: Strong";
-        strengthText.style.color = "green"
+        strengthText.style.color = "darkgreen";
     }
 });
 
-const isOnlyLetters = (Text) => {
-    for (let char of Text) {
-        if (!isNaN(char)) {
+const isOnlyLetters = (text) => {
+    for (let char of text) {
+        if (char >= "0" && char <= "9") {
             return false;
         }
     }
     return true;
-}
+};
