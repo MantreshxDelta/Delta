e54e2349be6f0fff605389a852acd88832ef591b
Liladhar <mantresh19@gmail.com>
2026-01-14 19:30:46 +0000

    Add Express dependency and fix server import

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`app is listening on port ${port}`);
+});
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// })
