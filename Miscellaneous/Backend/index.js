b52feed70d105a00f92086791278c609bcf3ec87
Liladhar <mantresh19@gmail.com>
2026-01-18 21:38:17 +0000

    !

---
+++ Miscellaneous/Backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/register", (req, res) => {
+    let {user, password} = req.query;
+    res.send(`Standard Get response. Welcome ${user}`)
+})
+
+app.post("/register", (req, res) => {
+    res.send("Standard Post response")
+})
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`)
+})
