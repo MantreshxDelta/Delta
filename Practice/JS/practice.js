8a2e86b94ef2e8727d1258f4719414be2d9d6402
Liladhar <mantresh19@gmail.com>
2026-01-18 16:30:08 +0000

    cc

--- Practice/JS/practice.js
+++ Practice/JS/practice.js
@@ -1,7 +1,25 @@
-let btn = document.getElementById("btn");
-let count = document.getElementById("count");
-count = 0;
-    btn.addEventListener('click', () => {
-        count++;
-        document.getElementById("count").innerText = count;
-    });
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+
+const addbtn = document.getElementById("add");
+const subbtn = document.getElementById("sub");
+const mulbtn = document.getElementById("mul");
+
+const output = document.getElementById("output");
+
+addbtn.addEventListener("click", () => {
+    const a = Number(num1.value);
+    const b = Number(num2.value);
+    output.textContent = a + b;
+})
+subbtn.addEventListener("click", () => {
+    const a = Number(num1.value);
+    const b = Number(num2.value);
+    output.textContent = a - b;
+});
+
+mulbtn.addEventListener("click", () => {
+    const a = Number(num1.value);
+    const b = Number(num2.value);
+    output.textContent = a * b;
+});
