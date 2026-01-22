da6c86150b76016167b196a8ee03381e0ed3baa7
Liladhar <mantresh19@gmail.com>
2026-01-22 12:40:30 +0000

    Cc

--- Backend/Rest/index.js
+++ Backend/Rest/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-import { v4 as uuidv4 } from 'uuid';
-uuidv4();
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride("_method"))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,17 +15,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id: "1a",
+        id: uuidv4(),
         username: "apnacollege",
         content: "I love coding!"
     }, 
     {
-        id: "2a",
+        id: uuidv4(),
         username: "shradhakhapra",
         content: "Hardwork is important to achieve success",
     },
     {
-        id: "3a",
+        id: uuidv4(),
         username: "Mantresh",
         content: "I am working as a SE in Google!"
     }
@@ -41,7 +42,8 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res)=> {
     let {username, content} = req.body
-    posts.push({username, content})
+    let id = uuidv4()
+    posts.push({id, username, content})
     res.redirect("/posts")
 })
 
@@ -50,6 +52,21 @@ app.get("/posts/:id", (req, res)=> {
     let post = posts.find((p) => id === p.id)
      if (!post) return res.status(404).send("Post not found");
     res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id)
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("edit.ejs", {post})
 })
 
 app.listen(port, () => {
