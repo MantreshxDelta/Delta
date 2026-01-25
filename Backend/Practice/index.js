110f6a364b9157556a42d8ceaf0f60260f0e330c
Liladhar <mantresh19@gmail.com>
2026-01-25 18:40:11 +0000

    practice

--- Backend/Practice/index.js
+++ Backend/Practice/index.js
@@ -7,8 +7,14 @@ app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
 
-app.get("/home", (req, res) => {
-    console.log("New incomming request")
-    let code = "<h1>Fruits</h1>"
-    res.send(code)
+// app.get("/:home", (req, res) => {
+//     console.log("New incomming request")
+//     let code = "<h1>Fruits</h1>"
+//     res.send(code)
+// })
+
+app.get("/:username/:id", (req, res) => {
+    let {username, id} = req.params
+    res.send(`this is the page of @${username}`)
 })
+
