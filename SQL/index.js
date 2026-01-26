125083f1000c00dbb52168d663edffd725fcba31
Liladhar <mantresh19@gmail.com>
2026-01-26 17:28:01 +0000

    SQL

--- SQL/index.js
+++ SQL/index.js
@@ -1,4 +1,23 @@
 const { faker } = require('@faker-js/faker');
+// Get the client
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: 'SDE@f66ng'
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if(err) throw err;
+    console.log(result)
+  })
+} catch (err) {
+  console.log(err)
+}
 
 let getRandomUser= () => {
   return {
